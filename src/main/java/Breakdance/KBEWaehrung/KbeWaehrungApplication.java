5531ccea47c88945a326acb65ff6bb385f1516f1
Thien Duong <duongthien1809@gmail.com>
2023-02-03 22:22:02 +0100

    first commit

---
+++ src/main/java/Breakdance/KBEWaehrung/KbeWaehrungApplication.java
@@ -0,0 +1,13 @@
+package Breakdance.KBEWaehrung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeWaehrungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbeWaehrungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Breakdance/KBEWaehrung/KbeWaehrungApplicationTests.java
@@ -0,0 +1,13 @@
+package Breakdance.KBEWaehrung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbeWaehrungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
